687178f2990c00a40ca07c4fb5fa52471f7101fb
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2015-08-05 16:31:18 +0200

    new test cases

---
+++ examples/boxes/2D_xz_advection_tri/OGS_FEM/run.bat
@@ -0,0 +1,7 @@
+del F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM\*.txt
+del F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM\*.tec
+del F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM\*.asc
+
+echo RUNNING boxes 2D_xz_advection_tri OGS_FEM
+ 
+F:\ogs\ogs_kb1\sources\Build_OGS_FEM\bin\Release\ogs F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM\testCase > F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM\out.txt
