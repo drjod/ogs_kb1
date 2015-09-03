21094dc4812ee0aca19f7dea2072e67e1a075aa5
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2015-09-03 16:41:05 +0200

    update

--- examples/boxes/2D_xz_advection_tri/OGS_FEM_SP/run.bat
+++
@@ -1,7 +0,0 @@
-del F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM_SP\*.txt
-del F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM_SP\*.tec
-del F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM_SP\*.asc
-
-echo RUNNING boxes 2D_xz_advection_tri OGS_FEM_SP
- 
-F:\ogs\ogs_kb1\sources\Build_OGS_FEM_SP\bin\Release\ogs F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM_SP\testCase > F:\ogs\ogs_kb1\examples\boxes\2D_xz_advection_tri\OGS_FEM_SP\out.txt
